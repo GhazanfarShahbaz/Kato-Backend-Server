0255c88a2d2f9116c01f264fe4df106a3881a8d2
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-04-22 02:23:23 -0400

    literal typing

--- kato_chat_bot/model_resource.py
+++ kato_chat_bot/model_resource.py
@@ -1,17 +1,19 @@
 import falcon
 
+from typing import List
+
 from kato_chat_bot.model.model_handler import Model_Handler
 
-import json
 class Model_Resource:
     def __init__(self):
-        self.model_handler = Model_Handler()
+        self.model_handler: Model_Handler = Model_Handler()
     
     def on_post(self, req, resp):
-        form = req.get_media()
+        form: List[any] = req.get_media()
         
         function_mapper_file_bytes = None 
-        request_json = {}
+        version_string: str = ""
+        intents: dict = {}
         
         for part in form:
             if part.name == "file":
@@ -19,9 +21,9 @@ class Model_Resource:
             
             if part.name == 'json':
                 request_json = part.get_media()
-        
-        version_string = request_json["version"]
-        intents = request_json["intents"]
+                
+                version_string = request_json["version"]
+                intents = request_json["intents"]
         
         self.model_handler.create_and_save_model(version_string, intents, function_mapper_file_bytes)
         resp.status = falcon.HTTP_201
